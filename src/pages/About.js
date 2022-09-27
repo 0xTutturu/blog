7fde9cf92aa8ef361a11dfc0c1650a12d21a9df5
0xTutturu <tutturutech@gmail.com>
2022-09-27 20:34:18 +0200

    changed about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,22 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
-import { TypeAnimation } from "react-type-animation";
 
 const About = () => {
-	const sentences = [
-		`Hi, I'm 0xTutturu`,
-		`I'm a freelance full stack and smart contract developer`,
-		`I do audit contests on code4rena and Sherlock, as well as `,
-	];
-
-	const addParagraph = (text) => {
-		const para = document.createElement("h4");
-		const node = document.createTextNode(text);
-		para.appendChild(node);
-		const element = document.getElementById("permanent");
-		element.appendChild(para);
-	};
-
 	return (
 		<div className='flex flex-col bg-base h-screen'>
 			<Header topic={`>> /`} content={`About_Me`} />
