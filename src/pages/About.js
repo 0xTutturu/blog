ebefaf0011025a1f98604fa0434e1302b8f0b113
0xTutturu <tutturutech@gmail.com>
2022-09-27 20:32:39 +0200

    about me updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,39 +1,31 @@
 import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
+import { TypeAnimation } from "react-type-animation";
 
 const About = () => {
-	const [typed, setTyped] = useState("");
-
 	const sentences = [
 		`Hi, I'm 0xTutturu`,
-		`I do smart contract development using Solidity and web development using React and TailwindCSS`,
+		`I'm a freelance full stack and smart contract developer`,
+		`I do audit contests on code4rena and Sherlock, as well as `,
 	];
 
-	useEffect(() => {
-		const timeout = setTimeout(() => {
-			if (typed.length < sentences[0].length) {
-				setTyped(sentences[0].slice(0, typed.length + 1));
-			} else {
-				setTyped(
-					typed.slice(0, sentences[0].length) +
-						sentences[1].slice(0, typed.length - sentences[0].length + 1)
-				);
-			}
-		}, 25);
-
-		return () => clearTimeout(timeout);
-	}, [typed]);
+	const addParagraph = (text) => {
+		const para = document.createElement("h4");
+		const node = document.createTextNode(text);
+		para.appendChild(node);
+		const element = document.getElementById("permanent");
+		element.appendChild(para);
+	};
 
 	return (
 		<div className='flex flex-col bg-base h-screen'>
 			<Header topic={`>> /`} content={`About_Me`} />
-			<div className='flex flex-col mx-auto w-8/12 text-textGreen pt-5'>
-				<h2 className='text-2xl text-textGreen mb-0'>
-					{`>> ` + typed.slice(0, sentences[0].length)}
-				</h2>
-				<p className='text-2xl m-0'>
-					{`>> ` + typed.slice(sentences[0].length, typed.length)}
-				</p>
+			<div className='flex flex-col mx-auto w-8/12 text-textGreen text-2xl pt-10'>
+				<h4 className='pb-2'>Hi! I'm 0xTutturu.</h4>
+				<h4 className='pb-2'>
+					I'm a freelance full stack and smart contract developer that does
+					audit contests on code4rena and Sherlock from time to time.
+				</h4>
 			</div>
 		</div>
 	);
